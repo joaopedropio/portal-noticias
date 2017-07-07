7e1cf18b389b1d200b5d2ce13ba8fa80aa0af670
joaopedropio <joao-pio@hotmail.com>
2017-07-07 14:02:20 -0300

    fixing first commit

--- noticias.js
+++ noticias.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var express = require('express');
 
 var port = 3000;
 
 var server = http.createServer(function (req, res) {
-	res.end("i");
+	res.end("Hello");
 }).listen(port);
 
 console.log("rodando na porta " + port);
