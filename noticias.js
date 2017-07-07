213b104069e37f05550a0e3fb2207ea2b4a1b5ca
joaopedropio <joao-pio@hotmail.com>
2017-07-07 14:20:02 -0300

    npm install used

--- noticias.js
+++ noticias.js
@@ -4,11 +4,15 @@ var express = require('express');
 var port = 3000;
 
 var server = http.createServer(function (req, res) {
-	res.end("Hello");
+	var categoria = req.url;
+
+	if(categoria == '/parabens') {
+		res.end("<html><body><p> m</p><body/><html>");
+	}
 }).listen(port);
 
 console.log("rodando na porta " + port);
 
 var livereload = require('livereload');
-var server = livereload.createServer({exts: ['ejs']});
+var server = livereload.createServer({exts: ['ejs', 'js']});
 server.watch(__dirname + "/");
